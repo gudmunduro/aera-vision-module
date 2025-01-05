11f6fc7adf9c68d2a5fb6a7340de6c6d8a555605
gudmunduro <goh00@protonmail.com>
2025-01-05 15:55:18 +0000

    Support building on macos

--- pixy2/build.rs
+++ pixy2/build.rs
@@ -1,11 +1,13 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         .compiler("g++")
-        .include("/usr/include/libusb-1.0")
-        .include("/home/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/pixy2/src/host/libpixyusb2/include")
-        .include("/home/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/pixy2/src/host/arduino/libraries/Pixy2")
-        .include("/home/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/src")
-        .object("/home/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/src/bridge.o")
+        .std("c++17")
+        .include("/opt/homebrew/include/")
+        .include("/opt/homebrew/include/libusb-1.0/")
+        .include("/Users/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/pixy2/src/host/libpixyusb2/include")
+        .include("/Users/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/pixy2/src/host/arduino/libraries/Pixy2")
+        .include("/Users/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/src")
+        .object("/Users/gudmundur/Projects/HR/Thesis/aera-vision-module/pixy2/src/bridge.o")
         .compile("pixy2");
     
     println!("cargo:rustc-link-lib=usb-1.0");
